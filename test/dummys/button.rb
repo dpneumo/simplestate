ef03ae7fc777edacb94ce15fd09e357c2033bf05
Mitchell C Kuppinger <dpneumo@gmail.com>
2016-01-29 02:26:38 -0600

    The button module provides an example of the usage of Simplestate. Tests of this are provided in simplestate_test.rb.

---
+++ test/dummys/button.rb
@@ -0,0 +1,51 @@
+module Button
+  class On < State
+    def press
+      transition_to(Off)
+      holder.messages << "#{holder.button_name} is off"
+    end
+
+    def name
+      'On'
+    end
+
+  private
+    def enter
+      holder.messages << "Entered the On state"
+    end
+
+    def exit
+      holder.messages << "Exited the On state"
+    end
+  end
+
+  class Off < State
+    def press
+      transition_to(On)
+      holder.messages << "#{holder.button_name} is on"
+    end
+
+    def name
+      'Off'
+    end
+
+  private
+    def enter
+      holder.messages << "Entered the Off state"
+    end
+
+    def exit
+      holder.messages << "Exited the Off state"
+    end
+  end
+
+  class Button < StateHolder
+    def button_name
+      "Button"
+    end
+
+    def messages
+      @messages ||= []
+    end
+  end
+end
--- test/simplestate_test.rb
+++ test/simplestate_test.rb
@@ -1,12 +1,56 @@
 require 'test_helper'
+require 'dummys/button'
 
 class SimplestateTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Simplestate::VERSION
   end
 
-  def test_it_does_something_useful
-    skip
-    assert false
+  include Button
+
+  def setup
+    @button = Button.new(initial_state_class: Off)
+  end
+
+  def test_initial_button_state_is_set
+    assert_equal 'Off', @button.current_state.name
+    assert_equal  nil,  @button.previous_state_class
+  end
+
+  def test_single_button_press
+    @button.press
+    expected = ["Exited the Off state", "Entered the On state", "Button is on"]
+    assert_equal expected, @button.messages
+    assert_equal Off, @button.previous_state_class
+  end
+
+  def test_double_button_press
+    @button.press
+    @button.press
+    expected = [ "Exited the Off state",
+                 "Entered the On state",  "Button is on", "Exited the On state",
+                 "Entered the Off state", "Button is off" ]
+    assert_equal expected, @button.messages
+    assert_equal On, @button.previous_state_class
+  end
+
+  def test_a_button_does_not_respond_to_enter
+    assert_equal false, @button.respond_to?(:enter)
+  end
+
+  def test_a_button_does_not_respond_to_exit
+    assert_equal false, @button.respond_to?(:exit)
+  end
+
+  def test_a_button_does_not_respond_to_a_private_method
+    assert_equal false, @button.respond_to?(:private_method)
+  end
+
+  def test_a_button_responds_to_press
+    assert_equal true, @button.respond_to?(:press)
+  end
+
+  def test_raises_NoMethodError_
+    assert_equal false, @button.respond_to?(:private_method)
   end
 end
